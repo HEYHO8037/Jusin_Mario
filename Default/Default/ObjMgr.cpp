4ab6f85880bf72353559be22f7b06ba0db8c8940
DESKTOP-TMLO51S\kje29 <kje293@gmail.com>
2022-05-02 16:43:29 +0900

    정리

--- Default/Default/ObjMgr.cpp
+++ Default/Default/ObjMgr.cpp
@@ -46,12 +46,9 @@ void CObjMgr::Add_Object(OBJID eID, CObj* pObj)
 		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(1480.f, 575.f, TYPE_HUR_FLOAT, pObj));
 
 		//움직이는 허들을 통해 점프하기
-		//정은test로주석걸엇어요
-    	//m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(1730.f, 525.f, TYPE_HUR_FIXED, pObj));
+    	m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(1730.f, 525.f, TYPE_HUR_FIXED, pObj));
 		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(1730.f, 575.f, TYPE_HUR_FIXED, pObj));
 		
-
-
 		//아이템 허들과 안움직이는 허들 ( 아이템 먹기 )
 		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(1980.f, 425.f, TYPE_HUR_FIXED, pObj));
 		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(2030.f, 425.f, TYPE_HUR_ITEM, pObj));
@@ -69,10 +66,10 @@ void CObjMgr::Add_Object(OBJID eID, CObj* pObj)
 		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(2730.f, 525.f, TYPE_HUR_FIXED, pObj));
 		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(2730.f, 575.f, TYPE_HUR_FIXED, pObj));
 		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(2730.f, 475.f, TYPE_HUR_FIXED, pObj));
-		//굴뚝
-		m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(2030.f, 575.f, TYPE_HUR_STACK, pObj));
-		//Boss Create(임시)
-
+		
+		//굴뚝(임시)
+		//m_ObjList[OBJ_HURDLE].push_back(CAbstractFactory<CHurdle>::Create(2030.f, 575.f, TYPE_HUR_STACK, pObj));
+		//보스(임시)
 		m_ObjList[OBJ_MONSTER].push_back(CAbstractFactory<CBossMonster>::Create(600.f, 575.f, TYPE_BOSS, pObj));
 
 	}
